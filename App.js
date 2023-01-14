eb8fec60fd68f8c4e53aba7e8361f842d1d28489
Kevin Straube <kevinstraube97@gmail.com>
2023-01-14 14:23:04 -0500

    Built app with necessary dependencies

--- App.js
+++ App.js
@@ -1,20 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View className="flex-1 justify-center items-center">
+      <Ionicons name="golf" size={30} color="green" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
