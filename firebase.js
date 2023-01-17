7566856481618b8ce4d903f487475ac3cef4c362
Kevin Straube <kevinstraube97@gmail.com>
2023-01-17 13:06:39 -0500

    Added firebase

---
+++ firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+
+const firebaseConfig = {
+apiKey: "AIzaSyB2_QsBxQTljoHTVv9mn-u3nqD0qTYDjVE",
+  authDomain: "golfapp-ca492.firebaseapp.com",
+  projectId: "golfapp-ca492",
+  storageBucket: "golfapp-ca492.appspot.com",
+  messagingSenderId: "1013459442897",
+  appId: "1:1013459442897:web:3cf1564b1e72096653f9f0"
+};
+
+const app = initializeApp(firebaseConfig);
