63df394b51f1e2ce3cfe0df96c44718d0acbaa44
Kevin Straube <kevinstraube97@gmail.com>
2023-02-06 18:29:43 -0500

    setup matching algorithm function

--- Pages/HomePage.js
+++ Pages/HomePage.js
@@ -1,10 +1,10 @@
 import React, { useEffect, useLayoutEffect, useRef, useState } from "react";
-import { View, Text, FlatList, SafeAreaView, Image, TouchableOpacity } from "react-native";
+import { View, Text, FlatList, SafeAreaView, Image, TouchableOpacity, StyleSheet, Alert } from "react-native";
 import { auth } from '../firebase';
 import { Ionicons, Entypo } from '@expo/vector-icons';
 import { useAuth } from '../hooks/useAuth';
 import Swiper from 'react-native-deck-swiper';
-import { onSnapshot, getFirestore, doc, collection, setDoc, getDocs, query, where } from "firebase/firestore";
+import { onSnapshot, getFirestore, doc, collection, setDoc, getDocs, query, where, getDoc } from "firebase/firestore";
 
 const db = getFirestore();
 
@@ -26,12 +26,17 @@ const HomePage = () => {
                     ((snapshot) => snapshot.docs.map((doc) => doc.id)
                 );
 
+                const likes = await getDocs(collection(db, 'users', user.uid, 'likes')).then
+                    ((snapshot) => snapshot.docs.map((doc) => doc.id)
+                );
+
                 const passedUserIds = passes.length > 0 ? passes : ["test"];
+                const likedUserIds = likes.length > 0 ? likes : ["test"];
 
                 unsubscribe = onSnapshot(
                     query(
                         collection(db, 'users'),
-                        where('uid', 'not-in', [...passedUserIds])
+                        where('uid', 'not-in', [...passedUserIds, ...likedUserIds])
                     ),
                     (snapshot) => {
                         setProfiles(
@@ -68,7 +73,44 @@ const HomePage = () => {
     };
 
     const swipeRight = async (cardIndex) => {
+        if (!profiles[cardIndex]) return;
 
+        const userSwiped = profiles[cardIndex];
+        const loggedInUser = await (
+            await getDoc(db, 'users', user.uid)
+        ).data();
+
+        //Make checking match a cloud function later
+
+        getDoc(doc(db, 'users', userSwiped.id, 'swipes', user.uid)).then(
+            (documentSnapshot) => {
+                if (documentSnapshot.exists()) {
+                    //Other user has already swiped yes on you, create a match!
+                    console.log("It's a match!");
+
+                    setDoc(doc(db, 'users', user.uid, 'likes', userSwiped.uid),
+                    {
+                        uid: userSwiped.uid,
+                        firstName: userSwiped.firstName,
+                        age: userSwiped.age,
+                    });
+
+                    //Match
+                    
+
+                } else {
+                    //Other user has not swiped yes yet, create a new doc in likes 
+                    console.log(`You swiped YES on ${userSwiped.firstName}`);
+
+                    setDoc(doc(db, 'users', user.uid, 'likes', userSwiped.uid),
+                    {
+                        uid: userSwiped.uid,
+                        firstName: userSwiped.firstName,
+                        age: userSwiped.age,
+                    });
+                }
+            }
+        );
     }; 
 
     return (
@@ -77,22 +119,22 @@ const HomePage = () => {
                 className="h-14 w-14 self-center"
                 source={require('../assets/TransparentLogo.png')}
             />
+
             {/* Profile Cards */}
+
             <View className="flex-1 -mt-10 ">
                 <Swiper
                     ref={swipeRef}
                     containerStyle={{ backgroundColor: "transparent" }}
                     cards={profiles}
-                    cardIndex={0}
                     stackSize={5}
+                    cardIndex={0}
                     animateCardOpacity
                     verticalSwipe={false}
                     onSwipedLeft={(cardIndex) => {
-                        console.log("Swiped next");
                         swipeLeft(cardIndex);
                     }}
                     onSwipedRight={(cardIndex) => {
-                        console.log("Swiped yes");
                         swipeRight(cardIndex);
                     }}
                     overlayLabels={{
@@ -115,15 +157,15 @@ const HomePage = () => {
                             },
                         },
                     }}
-                    renderCard={card => card ? (
-                            <View key={card.id} className="bg-white h-3/4 rounded-xl">
+                    renderCard={(card) => card ? (
+                            <View key={card.uid} className="relative bg-white h-3/4 rounded-xl">
                                 <View className="top-0 h-1/2 w-full rounded-xl">
                                     <FlatList 
                                         data={card.images} 
                                         renderItem={(item) => {
                                             return (
-                                                <View className="">
-                                                    <Image className="w-96 h-full self-center" source={{uri: item.item.url}} />
+                                                <View className="rounded-xl">
+                                                    <Image className="w-96 h-full self-center rounded-t-xl" source={{uri: item.item.url}} />
                                                 </View>
                                             )
                                         }}
@@ -136,7 +178,6 @@ const HomePage = () => {
                                 <View className="bg-white w-full h-20 rounded-b-xl">
                                     <View>
                                         <Text className="font-bold">{card.firstName}, {card.age}</Text>
-                                        
                                     </View>
                                     <View>
                                         <Text>{card.city}</Text>
@@ -148,7 +189,7 @@ const HomePage = () => {
                                 </View>
                             </View>
                         ) : (
-                            <View className="relative bg-white h-3/4 rounded-xl justify-center items-center">
+                            <View className="bg-white h-3/4 rounded-xl justify-center items-center" style={style.cardShadow}>
                                 <Text className="font-bold pb-5">No more profiles!</Text>
                                 <Text>Please try again later</Text>
                             </View>
@@ -175,3 +216,16 @@ const HomePage = () => {
 };
 
 export default HomePage;
+
+const style = StyleSheet.create({
+    cardShadow: {
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 1,
+        },
+        shadowOpacity: 0.2,
+        shadowRadius: 1.41,
+        elevation: 2,
+    }
+})
