14b3f0430d68ebd3b075ad54f88192258a8cb43a
Kevin Straube <kevinstraube97@gmail.com>
2023-02-22 10:21:23 -0500

    match action menu

--- Pages/MessagePage.js
+++ Pages/MessagePage.js
@@ -1,14 +1,15 @@
-import { View, Text, SafeAreaView, TouchableOpacity } from 'react-native'
+import { View, Text, SafeAreaView, TouchableOpacity, Alert } from 'react-native'
 import React, { useEffect, useRef, useState } from 'react'
 import getMatchedUserInfo from '../lib/getMatchedUserInfo';
 import { useAuth } from '../hooks/useAuth';
 import { useRoute } from '@react-navigation/native';
 import { Ionicons, Feather } from '@expo/vector-icons';
-import PopupMenu from './modals/PopupMenu';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import MatchChatPage from './matches/MatchChatPage';
 import MatchProfilePage from './matches/MatchProfilePage';
 import ActionSheet from 'react-native-actionsheet';
+import { onSnapshot, query, collection, doc, deleteDoc } from 'firebase/firestore';
+import { firestore } from '../firebase';
 
 const Tab = createMaterialTopTabNavigator();
 
@@ -18,7 +19,7 @@ const MessagePage = ({ navigation }) => {
     let actionSheet = useRef();
 
     let options = [
-        'Share Profile', 'Unmatch', 'Report', 'Cancel'
+        'Unmatch', 'Report', 'Cancel'
     ];
 
     const showActionSheet = () => {
@@ -65,9 +66,39 @@ const MessagePage = ({ navigation }) => {
             <ActionSheet
                 ref={actionSheet}
                 options={options}
-                cancelButtonIndex={3}
+                cancelButtonIndex={2}
+                destructiveButtonIndex={1}
                 onPress={(index) => {
-                    alert(options[index]);
+                    if (index === 0) {
+                        Alert.alert("Confirm", "Are you sure you want to remove the match?",[
+                            {
+                                text: "Cancel",
+                                style: "cancel",
+                            },
+                            {   /* TEST IF THIS FAILS WHEN NO MESSAGES HAVE BEEN SENT */
+                                //Delete all messages, then delete match 
+                                text: "Remove",
+                                onPress: (() => {
+                                    onSnapshot(
+                                        query(
+                                            collection(firestore, 'matches', matchDetails.id, 'messages'),
+                                        ),(snapshot) => 
+                                            snapshot.docs.forEach((snap) => (
+                                                deleteDoc(snap.ref)
+                                            )
+                                        )
+                                    )
+                                    deleteDoc(doc(firestore, 'matches', matchDetails.id));
+                                    navigation.navigate('Main');
+                                }),
+                            },
+                        ]);
+                    }
+                    else if (index === 1) {
+                        alert("REPORTED");
+                    } else {
+                        console.log("Cancelled");
+                    }
                 }}
             />
         </SafeAreaView>
