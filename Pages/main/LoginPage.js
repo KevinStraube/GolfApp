d9e4adc4e579eeed03ce9c8dc2b3eb336645deb1
Kevin Straube <kevinstraube97@gmail.com>
2023-04-01 11:56:10 -0400

    dependency re-install

--- Pages/main/LoginPage.js
+++ Pages/main/LoginPage.js
@@ -3,7 +3,6 @@ import { View, Text, TextInput, TouchableOpacity, Button, KeyboardAvoidingView, 
 import { SafeAreaView } from "react-native-safe-area-context";
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../../firebase';
-import { useNavigation } from "@react-navigation/native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const LoginPage = ({navigation}) => {
